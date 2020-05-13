70a9d4ec96e36d988882d7e6c167944bb3926087
dlckdgjs89 <dlckdgjs89@naver.com>
2020-05-13 18:41:13 +0900

    Day1

---
+++ Day1/02/ch02_test0.js
@@ -0,0 +1,9 @@
+console.log('안녕하세요.');
+
+console.log('반갑습니다.');
+
+console.log('고마워요.');
+
+console.log('숫자 보여주기 : %d', 10);
+console.log('문자 보여주기 : %s', '이창헌');
+console.log('JSON객체 보여주기 : %j', {name:'changhun'} );
